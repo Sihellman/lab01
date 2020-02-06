2a62cc07e93530c6c9f565614f45fe568b9d59b0
Sivia <sivia.hellman@ti.htc.edu>
2020-02-06 13:16:34 -0600

    initial commit

---
+++ src/main/java/edu/bi/foo/HelloWorld.java
@@ -0,0 +1,10 @@
+package edu.bi.foo;
+
+public class HelloWorld
+{
+    public static void main(String...args)
+    {
+        System.out.println("hello wrold");
+        System.exit(status: 47);
+    }
+}
